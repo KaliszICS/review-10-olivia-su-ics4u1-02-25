a2258badebe2058817cc695ab4105a663357261e
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-03-17 00:48:52 +0000

    kinda late oh well

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,54 @@ public class PracticeProblem {
 
 	}
 
+	public static String pyramid(int num){
+		String result = ""; 
+
+		for (int i = 1; i <= num; i++){
+			for (int j = 1; j <= (num - i); j++){
+			result += " ";
+			}
+			for (int k = 0; k < i; k++){
+				result += "*";
+				if (k != i - 1){
+					result += " ";
+				}
+			}
+			if ( i < num){
+				result += "\n";
+				}
+			}
+			return result;
+
+	}
+
+	public static String square(int num){
+		String result = "";
+		for (int i = 1; i <= num; i++){
+			for (int j = 1; j <= num; j++){
+				if (j == 1 || j == num || i == 1 || i == num){
+					result += "*";
+				}
+				else {
+					result += " ";
+				}
+			}
+			if (i < num){
+				result += "\n";
+			}
+		}
+		return result;
+	}
+
+	public static boolean hasLowercase(String word){
+		for (int i = 0; i < word.length(); i++){
+			if ((word.charAt(i) > 'a') && (word.charAt(i) < 'z')){
+				return true;
+			}
+		}
+		return false; 
+	}
+
+	// wahhhhhhhhhhhhhhhhhhhhhh
 }
+
